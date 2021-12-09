c660552ba1f53ce80db7938e01b1a39a27f0712b
Sam Briggs <samuel.briggs@depop.com>
2021-12-09 12:32:51 +0000

    Make port configurable

--- app/config/ApplicationSettings.scala
+++ app/config/ApplicationSettings.scala
@@ -3,13 +3,14 @@ package config
 import com.typesafe.config.Config
 import play.api.{ConfigLoader, Configuration}
 
-case class ApplicationSettings(signs: Seq[SignConfig])
+case class ApplicationSettings(signs: Seq[SignConfig], port: String)
 
 object ApplicationSettings {
   implicit val configLoader: ConfigLoader[ApplicationSettings] = (rootConfig: Config, path: String) => {
     val config = Configuration(rootConfig).get[Configuration](path)
     ApplicationSettings(
-      signs = config.get[Seq[SignConfig]]("signs")
+      signs = config.get[Seq[SignConfig]]("signs"),
+      port = config.get[String]("port")
     )
   }
 }
