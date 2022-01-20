bbd84af51a5a6c85e1f4676f0e6cfd9f9bf28ad6
Sam Briggs <samuel.briggs@depop.com>
2022-01-20 13:40:16 +0000

    Refactor packet to encapsulate image logic

--- app/models/packet/Packet.scala
+++ app/models/packet/Packet.scala
@@ -2,31 +2,23 @@ package models.packet
 
 import models.Image
 
-class Packet(
-    val command: Int,
-    val address: Int,
-    val payloadOption: Option[Seq[Int]] = None
-) {
-  val startByte = 0x02.toByte
-  val endByte = 0x03.toByte
+abstract class Packet(command: Int, address: Int) {
+  protected def payload: Seq[Int] = Seq.empty
 
   def bytes: Seq[Byte] = {
     require(command < scala.math.pow(2, 8))
     require(address < scala.math.pow(2, 8))
     // Start with a single byte for the command and address respectively
-    var data: Seq[Byte] =
-      Seq(command, address).flatMap(
-        HanoverByte(_, isPadded = false).toAsciiHex()
+    val headerBytes = {
+      Seq(command, address).map(
+        HanoverByte(_, isPadded = false)
       )
-    // Add the payload bytes, if necessary
-    payloadOption match {
-      case None => {}
-      case Some(payload) =>
-        data ++= payload.flatMap(HanoverByte(_).toAsciiHex())
     }
-    val checkSummedData = data :+ endByte
+    val payloadBytes = payload.map(HanoverByte(_))
+    val checkSummedData =
+      (headerBytes ++ payloadBytes).flatMap(_.toAsciiHex()) :+ Packet.endByte
     // Assemble together
-    (startByte +: checkSummedData) ++ HanoverByte(
+    (Packet.startByte +: checkSummedData) ++ HanoverByte(
       _calculateChecksum(checkSummedData)
     ).toAsciiHex()
   }
@@ -38,46 +30,52 @@ class Packet(
 }
 
 object Packet {
-  private def imageToInts(image: Image): Seq[Int] = {
-    // We pad each column to align with a bytes-worth of data
-    val byteAlignedRowCount = closestLargerMultiple(image.rows, 8)
-    val newImage = Image(
-      image.data ++ Vector.fill(
-        byteAlignedRowCount - image.rows,
-        image.columns
-      )(false))
-    // Interpret each column as a series of whole bytes
-    val data = imageToBits(newImage)
-      .grouped(8)
-      .map(
-        _.zipWithIndex
-          .foldLeft(0)((acc, kv) =>
-            kv match {
-              case (value, index) =>
-                if (value) acc + scala.math.pow(2, index).toInt else acc
-            }
-          )
+  val startByte: Byte = 0x02.toByte
+  val endByte: Byte = 0x03.toByte
+
+  object StartTestSigns extends Packet(command = 3, address = 0)
+  object StopTestSigns extends Packet(command = 12, address = 0)
+
+  case class DrawImage(address: Int, image: Image)
+      extends Packet(command = 1, address = address) {
+
+    protected override def payload: Seq[Int] = {
+      // We pad each column to align with a bytes-worth of data
+      val byteAlignedRowCount = closestLargerMultiple(image.rows, 8)
+      val newImage = Image(
+        image.data ++ Vector.fill(
+          byteAlignedRowCount - image.rows,
+          image.columns
+        )(false)
       )
-      .toSeq
-    // We prefix with the "resolution" of the data
-    // This is essentially a (possibly-truncated) byte count
-    (data.length & 0xff) +: data
-  }
+      // Interpret each column as a series of whole bytes
+      val data = imageToBits(newImage)
+        .grouped(8)
+        .map(
+          _.zipWithIndex
+            .foldLeft(0)((acc, kv) =>
+              kv match {
+                case (value, index) =>
+                  if (value) acc + scala.math.pow(2, index).toInt else acc
+              }
+            )
+        )
+        .toSeq
+      // We prefix with the "resolution" of the data
+      // This is essentially a (possibly-truncated) byte count
+      (data.length & 0xff) +: data
+    }
 
-  private def imageToBits(image: Image): Seq[Boolean] = {
-    for (
-      col <- 0 until image.columns;
-      row <- 0 until image.rows
-    )
-      yield image.data(row)(col)
-  }
+    private def imageToBits(image: Image): Seq[Boolean] = {
+      for (
+        col <- 0 until image.columns;
+        row <- 0 until image.rows
+      )
+        yield image.data(row)(col)
+    }
 
-  private def closestLargerMultiple(value: Int, base: Int) = {
-    (value.floatValue / base).ceil.toInt * base
+    private def closestLargerMultiple(value: Int, base: Int) = {
+      (value.floatValue / base).ceil.toInt * base
+    }
   }
-
-  object StartTestSigns extends Packet(3, 0)
-  object StopTestSigns extends Packet(12, 0)
-  case class DrawImage(override val address: Int, image: Image)
-      extends Packet(1, address, payloadOption = Some(imageToInts(image)))
 }
--- test/scala/models/packet/PacketsSuite.scala
+++ test/scala/models/packet/PacketsSuite.scala
@@ -6,14 +6,14 @@ import models.packet.Packet._
 class PacketsSuite extends munit.FunSuite {
   test("no payload packet bytes") {
     testPacketBytes(
-      new Packet(1, 2),
+      new Packet(command = 1, address = 2) {},
       Seq(0x02, '1', '2', 0x03, '9', 'A')
     )
   }
 
   test("with payload packet bytes") {
     testPacketBytes(
-      new Packet(1, 2, payloadOption = Some(Seq(3, 4, 5))),
+      new Packet(1, 2) { override def payload = Seq(3, 4, 5) },
       Seq(0x02, '1', '2', '0', '3', '0', '4', '0', '5', 0x03, '6', 'E')
     )
   }
