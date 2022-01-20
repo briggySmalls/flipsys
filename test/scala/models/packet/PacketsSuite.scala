240fdd79a4996f89f693ce187c5d5d9c6e1d3627
Sam Briggs <samuel.briggs@depop.com>
2022-01-20 15:45:19 +0000

    Rework packetsuite to use scalatest

--- test/scala/models/packet/PacketsSuite.scala
+++ test/scala/models/packet/PacketsSuite.scala
@@ -2,70 +2,70 @@ package models.packet
 
 import models.Image
 import models.packet.Packet._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.wordspec.AnyWordSpec
 
-class PacketsSuite extends munit.FunSuite {
-  test("no payload packet bytes") {
-    testPacketBytes(
-      new Packet(command = 1, address = 2) {},
-      Seq(0x02, '1', '2', 0x03, '9', 'A')
-    )
-  }
+class PacketsSuite
+    extends AnyWordSpec
+    with Matchers
+    with TableDrivenPropertyChecks {
 
-  test("with payload packet bytes") {
-    testPacketBytes(
-      new Packet(1, 2) { override def payload = Seq(3, 4, 5) },
-      Seq(0x02, '1', '2', '0', '3', '0', '4', '0', '5', 0x03, '6', 'E')
-    )
-  }
-
-  test("StartTestSigns packet bytes") {
-    testPacketBytes(
-      StartTestSigns,
-      Seq(0x02, '3', '0', 0x03, '9', 'A')
-    )
-  }
-
-  test("StopTestSigns packet bytes") {
-    testPacketBytes(
-      StopTestSigns,
-      Seq(0x02, 'C', '0', 0x03, '8', 'A')
-    )
-  }
-
-  test("simple image packet bytes") {
-    // Create an image
-    val img = Image(
-      Vector.tabulate(4, 2)((row, col) =>
-        if (row == 0 && col == 0) true else false
-      )
-    )
-    val packet = new DrawImage(1, img)
-    testPacketBytes(
-      packet,
-      Seq(0x02, '1', '1', '0', '2', '0', '1', '0', '0', 0x03, '7', '8')
-    )
-  }
-
-  test("tall image packet bytes") {
-    // Create an image
-    val img = Image(
-      Vector.tabulate(15, 2)((row, col) =>
-        if (row == 0 && col == 0) true
-        else if (row == 9 && col == 0) true
-        else false
-      )
-    )
-    val packet = new DrawImage(1, img)
-    testPacketBytes(
-      packet,
-      Seq(0x02, '1', '1', '0', '4', '0', '1', '0', '2', '0', '0', '0', '0',
-        0x03, 'B', '4')
-    )
-  }
-
-  def testPacketBytes(packet: Packet, bytes: Seq[Byte])(implicit
-      loc: munit.Location
-  ) = {
-    assertEquals(packet.bytes, bytes)
+  "A Packet" should {
+    "have bytes field" which
+      forAll(
+        Table(
+          ("example", "packet", "bytes"),
+          (
+            "no payload packet",
+            new Packet(command = 1, address = 2) {},
+            Seq(0x02, '1', '2', 0x03, '9', 'A')
+          ),
+          (
+            "small payload packet",
+            new Packet(1, 2) { override def payload = Seq(3, 4, 5) },
+            Seq(0x02, '1', '2', '0', '3', '0', '4', '0', '5', 0x03, '6', 'E')
+          ),
+          (
+            "start test signs packet",
+            StartTestSigns,
+            Seq(0x02, '3', '0', 0x03, '9', 'A')
+          ),
+          (
+            "stop test signs packet",
+            StopTestSigns,
+            Seq(0x02, 'C', '0', 0x03, '8', 'A')
+          ),
+          (
+            "simple image packet", {
+              val img = Image(
+                Vector.tabulate(4, 2)((row, col) =>
+                  if (row == 0 && col == 0) true else false
+                )
+              )
+              new DrawImage(1, img)
+            },
+            Seq(0x02, '1', '1', '0', '2', '0', '1', '0', '0', 0x03, '7', '8')
+          ),
+          (
+            "tall image packet", {
+              val img = Image(
+                Vector.tabulate(15, 2)((row, col) =>
+                  if (row == 0 && col == 0) true
+                  else if (row == 9 && col == 0) true
+                  else false
+                )
+              )
+              new DrawImage(1, img)
+            },
+            Seq(0x02, '1', '1', '0', '4', '0', '1', '0', '2', '0', '0', '0',
+              '0', 0x03, 'B', '4')
+          )
+        )
+      ) { (example, packet, bytes) =>
+        s"correctly represents $example" in {
+          packet.bytes shouldEqual bytes
+        }
+      }
   }
 }
